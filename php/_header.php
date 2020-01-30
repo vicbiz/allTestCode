5f772a32300cbf76378d3d1cce27fc7e47230de8
Jae Moon <vicbizmoon@hotmail.com>
2020-01-30 15:45:49 -0500

    added php bootstrap javascripts

--- php/_header.php
+++ php/_header.php
@@ -17,6 +17,8 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>PHP Test</title>
     <link rel="stylesheet" href="bootstrap.min.css" type="text/css">
+    <script src="jquery.min.js"></script>
+    <script src="bootstrap.min.js"></script>
 </head>
 <body>
 
